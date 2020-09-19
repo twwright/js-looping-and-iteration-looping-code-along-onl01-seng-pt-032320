60ef06fe66f2f399ac1d8a01da678ac122da5faf
Thomas Wright <twwright@gmail.com>
2020-09-19 17:30:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solutions in this file
 
 function writeCards(arr, event) {
+  const thankYouCards = [];
   for (let i = 0; i < arr.length; i++) {
     console.log(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
   }
