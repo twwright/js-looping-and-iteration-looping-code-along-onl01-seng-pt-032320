f54f0a935c2cab08815574bcccc7ce4923ab6807
Thomas Wright <twwright@gmail.com>
2020-09-19 17:29:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function writeCards(arr, event) {
   for (let i = 0; i < arr.length; i++) {
     console.log(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
   }
+  return arr;
 }
 
 function countDown(int) {
