5127f4b7a23f46f0dabef791c2906efe4027fc5f
Thomas Wright <twwright@gmail.com>
2020-09-19 17:30:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(arr, event) {
+  const thankYouCards = [];
+  for (let i = 0; i < arr.length; i++) {
+    thankYouCards.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouCards;
+}
+
+function countDown(int) {
+  while (int > 0) {
+    console.log(int--);
+  }
+  console.log(int)
+}
